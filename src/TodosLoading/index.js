a0ca8a61f69fff7ba3043941e473cd61f10b6a1a
JoseLGutierrez28 <gutierrezjoseluis265@gmail.com>
2023-10-05 13:05:50 -0500

    set class to className

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -6,32 +6,32 @@ function TodosLoading() {
   return (
     <div className='firstContainer'>
 
-      <div class="loaderListTodo">
-        <div class="box box0">
+      <div className="loaderListTodo">
+        <div className="box box0">
           <div></div>
         </div>
-        <div class="box box1">
+        <div className="box box1">
           <div></div>
         </div>
-        <div class="box box2">
+        <div className="box box2">
           <div></div>
         </div>
-        <div class="box box3">
+        <div className="box box3">
           <div></div>
         </div>
-        <div class="box box4">
+        <div className="box box4">
           <div></div>
         </div>
-        <div class="box box5">
+        <div className="box box5">
           <div></div>
         </div>
-        <div class="box box6">
+        <div className="box box6">
           <div></div>
         </div>
-        <div class="box box7">
+        <div className="box box7">
           <div></div>
         </div>
-        <div class="ground">
+        <div className="ground">
           <div></div>
         </div>
       </div>
